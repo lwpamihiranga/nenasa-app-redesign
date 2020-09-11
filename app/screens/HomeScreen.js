7bf0d827b51d55d566dd9edb63db1c4a43a2c05a
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-09-11 15:01:30 +0530

    init HomeScreen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function HomeScreen() {
-  return (
-    <View>
-      <Text>This is the HomeScreen</Text>
-    </View>
-  );
+export function HomeScreen({ navigation }) {
+	return (
+		<View style={styles.container}>
+			<Button
+				onPress={() => navigation.navigate('Test')}
+				title="Go to Test Screen"
+			/>
+		</View>
+	);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
