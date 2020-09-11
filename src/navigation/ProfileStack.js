8d0273ac8a1d2f4a0e40e43e1ddc0176ddcfd62f
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-09-11 16:24:49 +0530

    hamburger menu button added to header

--- src/navigation/ProfileStack.js
+++ src/navigation/ProfileStack.js
@@ -2,13 +2,30 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { ProfileScreen } from '../screens/ProfileScreen';
 import { TestScreen } from '../screens/TestScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 
-export function ProfileStack() {
+export function ProfileStack({ navigation }) {
 	return (
 		<Stack.Navigator>
-			<Stack.Screen name="Profile" component={ProfileScreen} />
+			<Stack.Screen
+				name="Profile"
+				component={ProfileScreen}
+				options={{
+					headerLeft: () => {
+						return (
+							<Ionicons
+								name="md-menu"
+								size={30}
+								color="black"
+								onPress={() => navigation.openDrawer()}
+							/>
+						);
+					},
+					headerLeftContainerStyle: { paddingLeft: 10 },
+				}}
+			/>
 			<Stack.Screen name="Test" component={TestScreen} />
 		</Stack.Navigator>
 	);
