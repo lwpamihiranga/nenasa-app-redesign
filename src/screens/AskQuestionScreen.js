2c61d0ccf3fa03e7c5c1d7f8ac7724bca599c8c8
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-09-13 12:44:50 +0530

    indentation formatting fix

--- src/screens/AskQuestionScreen.js
+++ src/screens/AskQuestionScreen.js
@@ -1,92 +1,118 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button,TextInput,Alert,TouchableOpacity} from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	TextInput,
+	Alert,
+	TouchableOpacity,
+} from 'react-native';
 import { Dropdown } from 'react-native-material-dropdown-v2';
-import { AntDesign } from '@expo/vector-icons'; 
-
+import { AntDesign } from '@expo/vector-icons';
 
 export function AskQuestionScreen({ navigation }) {
-    let data = [{
-        value: 'grade 6',
-      }, {
-        value: 'grade 7',
-      }, {
-        value: 'grade 8',
-      },{
-        value: 'grade 9',
-      }, {
-        value: 'grade 10',
-      }, {
-        value: 'grade 11',
-      },{
-        value: 'History',
-      }, {
-        value: 'English',
-      }, {
-        value: 'Sinhala',
-      },{
-        value: 'Chemistry',
-      }, {
-        value: 'Physics',
-      }, {
-        value: 'Biology',
-      }];
-      const [value, onChangeText] = React.useState('');
+	let data = [
+		{
+			value: 'grade 6',
+		},
+		{
+			value: 'grade 7',
+		},
+		{
+			value: 'grade 8',
+		},
+		{
+			value: 'grade 9',
+		},
+		{
+			value: 'grade 10',
+		},
+		{
+			value: 'grade 11',
+		},
+		{
+			value: 'History',
+		},
+		{
+			value: 'English',
+		},
+		{
+			value: 'Sinhala',
+		},
+		{
+			value: 'Chemistry',
+		},
+		{
+			value: 'Physics',
+		},
+		{
+			value: 'Biology',
+		},
+	];
+	const [value, onChangeText] = React.useState('');
 
-   
 	return (
-        
-        
 		<View style={styles.container}>
-           
-            <Dropdown style={styles.dropdow}
-            label='Select grade or lesson'
-            data={data}
-            />
-           
-           <TextInput 
-            style={styles.textQ}
-            onChangeText={text => onChangeText(text)}
-            value={value}
-            placeholder="Write here......"
-            numberOfLines={2}
-            multiline={false}        
-            />
+			<Dropdown
+				style={styles.dropdow}
+				label="Select grade or lesson"
+				data={data}
+			/>
 
-            <TouchableOpacity
-                style={{ flex: 1 ,maxHeight:100,marginTop:20,marginLeft: 20,marginRight:20}}
-                >
-                <View
-                    style={{
-                        flex: 1,
-                        backgroundColor: '#d3d3d3',
-                        marginRight: 5,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}>
-                    <AntDesign name="upload" size={24} color="black" /> 
-                    
-                    <Text>upload a Image</Text>
-                    <Text>(Optional)</Text>
-                </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={{ flex: 1 ,maxHeight:60,marginTop:20,marginLeft: 20,marginRight:20}}
-                onPress={() => Alert.alert('Successfully submitted')}
+			<TextInput
+				style={styles.textQ}
+				onChangeText={(text) => onChangeText(text)}
+				value={value}
+				placeholder="Write here......"
+				numberOfLines={2}
+				multiline={false}
+			/>
 
-                >
-                <View
-                    style={{
-                        flex: 1,
-                        backgroundColor: '#F7BE56',
-                        marginRight: 5,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}>
-                    
-                    <Text style={{color:'#f0f8ff',fontSize:18}}>Submit</Text>
-                </View>
-            </TouchableOpacity>
-           
+			<TouchableOpacity
+				style={{
+					flex: 1,
+					maxHeight: 100,
+					marginTop: 20,
+					marginLeft: 20,
+					marginRight: 20,
+				}}>
+				<View
+					style={{
+						flex: 1,
+						backgroundColor: '#d3d3d3',
+						marginRight: 5,
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}>
+					<AntDesign name="upload" size={24} color="black" />
+
+					<Text>upload a Image</Text>
+					<Text>(Optional)</Text>
+				</View>
+			</TouchableOpacity>
+			<TouchableOpacity
+				style={{
+					flex: 1,
+					maxHeight: 60,
+					marginTop: 20,
+					marginLeft: 20,
+					marginRight: 20,
+				}}
+				onPress={() => Alert.alert('Successfully submitted')}>
+				<View
+					style={{
+						flex: 1,
+						backgroundColor: '#F7BE56',
+						marginRight: 5,
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}>
+					<Text style={{ color: '#f0f8ff', fontSize: 18 }}>
+						Submit
+					</Text>
+				</View>
+			</TouchableOpacity>
 		</View>
 	);
 }
@@ -95,25 +121,22 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		
-    },
-    dropdow:{
-        marginLeft:20,
-        marginRight:20,
-        marginTop:50,
-        height:80
-    },
-    textQ:{
-        marginLeft:20,
-        marginRight:20,
-        marginTop:20,
-        borderBottomColor: '#000000',
-        borderBottomWidth: 1,
-        height: 300,
-        borderColor: 'gray',
-        borderWidth: 1,
-        textAlign: 'justify'
-    },
-    
-   
+	},
+	dropdow: {
+		marginLeft: 20,
+		marginRight: 20,
+		marginTop: 50,
+		height: 80,
+	},
+	textQ: {
+		marginLeft: 20,
+		marginRight: 20,
+		marginTop: 20,
+		borderBottomColor: '#000000',
+		borderBottomWidth: 1,
+		height: 300,
+		borderColor: 'gray',
+		borderWidth: 1,
+		textAlign: 'justify',
+	},
 });
