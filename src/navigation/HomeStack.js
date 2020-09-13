ea16cb613ee94ea56fa36f6f337f3581a772b6e7
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-09-13 16:38:44 +0530

    add new stack screens

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -3,7 +3,11 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/HomeScreen';
 import { TestScreen } from '../screens/TestScreen';
 import { OLSubjectsScreen } from '../screens/OLSubjectsScreen';
-import {ALSubjectsScreen} from '../screens/ALSubjectsScreen'
+import { ALSubjectsScreen } from '../screens/ALSubjectsScreen';
+import { SubjectQuestionsScreen } from '../screens/SubjectQuestionsScreen';
+import { AskQuestionScreen } from '../screens/AskQuestionScreen';
+import { AnswerQuestion } from '../screens/AnswerQuestion';
+import { ExamScreen } from '../screens/ExamScreen';
 import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
@@ -32,6 +36,21 @@ export function HomeStack({ navigation }) {
 			<Stack.Screen name="Test" component={TestScreen} />
 			<Stack.Screen name="O/L Subjects" component={OLSubjectsScreen} />
 			<Stack.Screen name="A/L Subjects" component={ALSubjectsScreen} />
+			<Stack.Screen
+				name="SubjectQuestionsScreen"
+				component={SubjectQuestionsScreen}
+				options={{ title: 'A/L Biology' }}
+			/>
+			<Stack.Screen
+				name="AskQuestionScreen"
+				component={AskQuestionScreen}
+			/>
+			<Stack.Screen name="AnswerQuestion" component={AnswerQuestion} />
+			<Stack.Screen
+				name="ExamScreen"
+				component={ExamScreen}
+				options={{ title: 'Exams & Tests' }}
+			/>
 		</Stack.Navigator>
 	);
 }
