08ed0ca481e25e65855896c918cc180cf25ed791
TereenPrasanga <52649917+TereenPrasanga@users.noreply.github.com>
2020-09-13 12:46:30 +0530

    Implementation of About Us & Terms and Conditions, Ask Questions (#8)
    * App.js for about us and terms for condition
    
    * package and yarn
    
    * About us stack and screen
    
    * terma and condition stack and screeen
    
    * ask question screen and stack
    
    * fix error package.json
    
    * app.js indentation formating
    
    * indentation formatting fix
    
    Co-authored-by: Amith Mihiranga <lwpamihiranga@gmail.com>

---
+++ src/screens/AboutUsScreen.js
@@ -0,0 +1,69 @@
+
+import React from 'react';
+import { StyleSheet, Text, View, Button,ScrollView } from 'react-native';
+
+export function AboutUsScreen({ navigation }) {
+	return (
+		<View style={styles.container}>
+            <ScrollView>
+			<Text style={styles.text}>
+                නැණස education application එක සකසා ඇත්තේ ශ්‍රි ලංකාවේ ඇති සියලුම අකාරයේ පාසල් මට්ටමේ, 
+                විශ්ව විද්‍යාල මට්ටමේ සහ අනෙකුත් පාඨමාලා වලට අදාළ විභාගයන් සහ ඇගයීම් කටයුතු සඳහා සහයක් 
+                ලබාගැනීමට අවශ්‍ය තාක්ෂණික පසුබිමක් සකසන application එකක් ලෙසින්ය{"\n"}{"\n"}
+            </Text>
+            <Text style ={styles.text}>
+                මෙහිදී ඔබට,{"\n"}{"\n"}
+            </Text>
+            <Text style ={styles.text}>
+                එක් එක් විෂයන්ට අදාළ පෙරහුරු ප්‍රශ්න පත්‍ර විභාග ප්‍රශ්නපත්‍ර සහ අනෙකුත් සියලුම අකාරයේ 
+                අධ්‍යාපනික ලේඛන පහසුවෙන් ලබා ගැනීමට සහ හුවමාරැ කරගැනීමට හැක.{"\n"}{"\n"}
+            </Text>
+            <Text style ={styles.text}>
+                මෙම app එක airvapps හි නිෂ්පාදනයක් වන අතර ඔබට app එක සම්බන්දයෙන් ඇති සියලුම
+                ගැටළු airvapps facebook පිටුවටත් airvapps@gmail.com email ලිපිනියටත් එවිය හැක.{"\n"}{"\n"}
+            </Text>
+            <Text style ={styles.text}> 
+                යම්කිසි විෂයක් හෝ නියමයක් සම්බන්දයෙන් ඇති ගැටළු ඉදිරිපත් කිරීමට හැකිය. 
+                එමගින් ඔබගේ අධ්‍යාපනික ගැටළු වලට කඩිනම් පිළිතුරු ලබාගත හැකිය.{"\n"}{"\n"}
+            </Text>
+            <Text style ={styles.text}> 
+                ශ්‍රී ලංකාවේ අධ්‍යාපන ක්ෂේත්‍රය නැංවීමට ඔබත් දායක වන්න.Nenasa 
+                education application is developed to get technical support for any kind of
+                exams, assignments, and any other educational purposes at the school level or 
+                university level in Sri Lanka.{"\n"}{"\n"}
+            </Text>
+            <Text style ={styles.text}> 
+                As a pre-launch in this update, the application contains only a 
+                few subjects of AL and OL at the school level. in future updates, the app 
+                will include every type of subject.{"\n"}{"\n"}
+            </Text>
+            <Text style ={styles.text}> 
+                you can easily find and share past papers,
+                assignments, notes, and any other documents that education-related..{"\n"}{"\n"}
+            </Text>
+            <Text style ={styles.text}> 
+                This app is developed and published by airvapps and
+                if you have any suggestions you can contact us via airvapps Facebook page or 
+                airvapps@gmail our official email.{"\n"}{"\n"}
+            </Text>
+            <Text style ={styles.text}> 
+                Support to develop Sri Lankan education.{"\n"}{"\n"}
+            </Text>
+            </ScrollView>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+        alignItems: 'center',
+        marginTop:30,
+        marginLeft:10,
+        marginRight:10
+		//justifyContent: 'center',
+    },
+    text:{
+        fontSize:16
+    },
+});
---
+++ src/screens/AskQuestionScreen.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	TextInput,
+	Alert,
+	TouchableOpacity,
+} from 'react-native';
+import { Dropdown } from 'react-native-material-dropdown-v2';
+import { AntDesign } from '@expo/vector-icons';
+
+export function AskQuestionScreen({ navigation }) {
+	let data = [
+		{
+			value: 'grade 6',
+		},
+		{
+			value: 'grade 7',
+		},
+		{
+			value: 'grade 8',
+		},
+		{
+			value: 'grade 9',
+		},
+		{
+			value: 'grade 10',
+		},
+		{
+			value: 'grade 11',
+		},
+		{
+			value: 'History',
+		},
+		{
+			value: 'English',
+		},
+		{
+			value: 'Sinhala',
+		},
+		{
+			value: 'Chemistry',
+		},
+		{
+			value: 'Physics',
+		},
+		{
+			value: 'Biology',
+		},
+	];
+	const [value, onChangeText] = React.useState('');
+
+	return (
+		<View style={styles.container}>
+			<Dropdown
+				style={styles.dropdow}
+				label="Select grade or lesson"
+				data={data}
+			/>
+
+			<TextInput
+				style={styles.textQ}
+				onChangeText={(text) => onChangeText(text)}
+				value={value}
+				placeholder="Write here......"
+				numberOfLines={2}
+				multiline={false}
+			/>
+
+			<TouchableOpacity
+				style={{
+					flex: 1,
+					maxHeight: 100,
+					marginTop: 20,
+					marginLeft: 20,
+					marginRight: 20,
+				}}>
+				<View
+					style={{
+						flex: 1,
+						backgroundColor: '#d3d3d3',
+						marginRight: 5,
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}>
+					<AntDesign name="upload" size={24} color="black" />
+
+					<Text>upload a Image</Text>
+					<Text>(Optional)</Text>
+				</View>
+			</TouchableOpacity>
+			<TouchableOpacity
+				style={{
+					flex: 1,
+					maxHeight: 60,
+					marginTop: 20,
+					marginLeft: 20,
+					marginRight: 20,
+				}}
+				onPress={() => Alert.alert('Successfully submitted')}>
+				<View
+					style={{
+						flex: 1,
+						backgroundColor: '#F7BE56',
+						marginRight: 5,
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}>
+					<Text style={{ color: '#f0f8ff', fontSize: 18 }}>
+						Submit
+					</Text>
+				</View>
+			</TouchableOpacity>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
+	dropdow: {
+		marginLeft: 20,
+		marginRight: 20,
+		marginTop: 50,
+		height: 80,
+	},
+	textQ: {
+		marginLeft: 20,
+		marginRight: 20,
+		marginTop: 20,
+		borderBottomColor: '#000000',
+		borderBottomWidth: 1,
+		height: 300,
+		borderColor: 'gray',
+		borderWidth: 1,
+		textAlign: 'justify',
+	},
+});
