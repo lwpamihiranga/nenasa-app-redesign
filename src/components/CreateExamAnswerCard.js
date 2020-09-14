7a307b3fec044ebaf881353c32612a54f4c57048
Upulie Han <53108781+UpulieHan@users.noreply.github.com>
2020-09-14 18:22:04 +0530

    Partially done. Add buttons need implementation. (#15)

---
+++ src/components/CreateExamAnswerCard.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { RadioButton, TextInput } from 'react-native-paper';
+
+export function CreateExamAnswerCard({ navigation, createExamAnswerCard }) {
+	const [text, setText] = useState('');
+
+	const onAnswerChange = (textValue) => {
+		setText(textValue);
+		createExamAnswerCard(text);
+	};
+
+	return (
+		<View style={styles.bottomSubCard}>
+			<RadioButton style={styles.radiobutton} />
+			<TextInput
+				style={styles.answerTextInput}
+				placeholder="Your answer here..."
+				placeholderTextColor="black"
+				onChangeText={onAnswerChange}
+			/>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	bottomSubCard: {
+		flexDirection: 'row',
+		marginTop: 10,
+	},
+	radiobutton: {},
+	answerTextInput: {
+		backgroundColor: '#E5E5E5',
+		fontSize: 18,
+		width: '90%',
+	},
+});
---
+++ src/screens/CreateExamScreen.js
@@ -0,0 +1,240 @@
+import React, { useState } from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	Alert,
+	TouchableOpacity,
+} from 'react-native';
+import { Dropdown } from 'react-native-material-dropdown-v2';
+import {
+	AntDesign,
+	MaterialCommunityIcons,
+	Ionicons,
+} from '@expo/vector-icons';
+import { Picker } from '@react-native-community/picker';
+import { CreateExamQuestionCard } from '../components/CreateExamQuestionCard';
+
+export function CreateExamScreen({ navigation }) {
+	const [title, setTitle] = useState('');
+	const [hour, setHour] = useState('');
+	const [min, setMin] = useState('');
+	const [level, setLevel] = useState('');
+	const [medium, setMedium] = useState('');
+
+	const onChangeTitle = (titleValue) => {
+		setTitle(titleValue);
+	};
+	const onChangeHour = (hourValue) => {
+		setHour(hourValue);
+	};
+	const onChangeMins = (minValue) => {
+		setMin(minValue);
+	};
+	const onChangeLevel = (levelValue) => {
+		setLevel(levelValue);
+	};
+	const onChangeMedium = (mediumValue) => {
+		setMedium(mediumValue);
+	};
+	return (
+		<View style={styles.container}>
+			<View style={styles.topHalf}>
+				<TextInput
+					style={styles.titleInput}
+					onChangeText={onChangeTitle}
+					placeholder="Exam Title"
+					placeholderTextColor="black"
+				/>
+				<View style={styles.hoursMins}>
+					<Picker
+						selectedValue={hour}
+						onValueChange={onChangeHour}
+						style={styles.hoursPicker}
+						mode="dropdown">
+						<Picker.Item label="Hours" value="-1" />
+						<Picker.Item label="1" value="1" />
+						<Picker.Item label="2" value="2" />
+						<Picker.Item label="3" value="3" />
+					</Picker>
+					<Ionicons
+						name="md-arrow-dropdown"
+						size={24}
+						color="black"
+						style={styles.smallLeftdropIcon}
+					/>
+					<Picker
+						selectedValue={min}
+						onValueChange={onChangeMins}
+						style={styles.minsPicker}>
+						<Picker.Item label="Mins" value="-1" />
+						<Picker.Item label="00" value="00" />
+						<Picker.Item label="05" value="05" />
+						<Picker.Item label="10" value="10" />
+						<Picker.Item label="15" value="15" />
+						<Picker.Item label="20" value="20" />
+						<Picker.Item label="25" value="25" />
+						<Picker.Item label="30" value="30" />
+						<Picker.Item label="35" value="35" />
+						<Picker.Item label="40" value="40" />
+						<Picker.Item label="45" value="45" />
+						<Picker.Item label="50" value="50" />
+						<Picker.Item label="55" value="55" />
+					</Picker>
+					<Ionicons
+						name="md-arrow-dropdown"
+						size={24}
+						color="black"
+						style={styles.smallRightdropIcon}
+					/>
+				</View>
+				<Picker
+					selectedValue={level}
+					onValueChange={onChangeLevel}
+					style={styles.levelsPicker}>
+					<Picker.Item label="Exam Level" value="-1" />
+					<Picker.Item label="O/L" value="O/L" />
+					<Picker.Item label="A/L" value="A/L" />
+					<Picker.Item label="Other" value="Other" />
+				</Picker>
+				<Ionicons
+					name="md-arrow-dropdown"
+					size={24}
+					color="black"
+					style={styles.bigTopdropIcon}
+				/>
+				<Picker
+					selectedValue={medium}
+					onValueChange={onChangeMedium}
+					style={styles.mediumPicker}>
+					<Picker.Item label="Medium" value="-1" />
+					<Picker.Item label="Sinhala" value="Sinhala" />
+					<Picker.Item label="Tamil" value="Tamil" />
+					<Picker.Item label="English" value="English" />
+				</Picker>
+				<Ionicons
+					name="md-arrow-dropdown"
+					size={24}
+					color="black"
+					style={styles.bigBottomdropIcon}
+				/>
+			</View>
+			<CreateExamQuestionCard />
+			<View style={styles.lowerHalf}>
+				<TouchableOpacity
+					style={styles.addQuestion}
+					onPress={() => Alert.alert('Successfully submitted')}>
+					<AntDesign name="pluscircle" size={24} color="black" />
+					<Text style={styles.buttonText}>Add a new Question</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					style={styles.saveExam}
+					onPress={() => Alert.alert('Successfully submitted')}>
+					<MaterialCommunityIcons
+						name="content-save-settings"
+						size={24}
+						color="black"
+					/>
+					<Text style={styles.buttonText}>Save Exam</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'white',
+	},
+	topHalf: {
+		height: 300,
+	},
+	titleInput: {
+		marginTop: 50,
+		borderWidth: 1,
+		textAlign: 'justify',
+		backgroundColor: '#E5E5E5',
+		borderColor: '#E5E5E5',
+		marginHorizontal: 10,
+		padding: 10,
+		fontSize: 16,
+	},
+	hoursMins: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginVertical: 10,
+	},
+	hoursPicker: {
+		flex: 1,
+		marginStart: 10,
+		backgroundColor: '#E5E5E5',
+		position: 'relative',
+	},
+	smallLeftdropIcon: {
+		position: 'absolute',
+		marginStart: 180,
+		marginTop: 13,
+	},
+	minsPicker: {
+		flex: 1,
+		marginHorizontal: 10,
+		backgroundColor: '#E5E5E5',
+		position: 'relative',
+	},
+	smallRightdropIcon: {
+		position: 'absolute',
+		marginStart: 380,
+		marginTop: 13,
+	},
+	levelsPicker: {
+		flex: 1,
+		backgroundColor: '#E5E5E5',
+		marginBottom: 10,
+		marginHorizontal: 10,
+		position: 'relative',
+	},
+	bigTopdropIcon: {
+		position: 'absolute',
+		marginTop: 185,
+		marginStart: 377,
+	},
+	mediumPicker: {
+		flex: 1,
+		backgroundColor: '#E5E5E5',
+		marginBottom: 10,
+		marginHorizontal: 10,
+		position: 'relative',
+	},
+	bigBottomdropIcon: {
+		position: 'absolute',
+		marginTop: 250,
+		marginStart: 377,
+	},
+	lowerHalf: {
+		flexDirection: 'row',
+		marginBottom: 10,
+		position: 'absolute',
+		bottom: 0,
+	},
+	addQuestion: {
+		width: '45%',
+		padding: 10,
+		marginTop: 20,
+		marginHorizontal: 10,
+		backgroundColor: '#FECC5D',
+		flexDirection: 'row',
+		justifyContent: 'space-evenly',
+	},
+	saveExam: {
+		width: '45%',
+		padding: 10,
+		marginTop: 20,
+		marginHorizontal: 10,
+		backgroundColor: '#FECC5D',
+		flexDirection: 'row',
+		justifyContent: 'space-evenly',
+	},
+	buttonText: { color: '#000', fontSize: 15 },
+});
